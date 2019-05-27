6e40d0a1a55b235aa860a7cb90bd0c756b951ff1
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-05-27 03:50:23 -0700

    Added Shooting

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public KeyCode shootKey;
+    public GameObject projectile;
+    public float projSpeed;
+    public float offset;
+    int layerMask = 1 << 9;
+
+    [SerializeField]
+    private Transform m_Transform;
+
+    [SerializeField]
+    private Vector3 m_MousePos;
+
+    [SerializeField]
+    private Vector3 m_ShootDir;
+
+    [SerializeField]
+    private bool debug;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Transform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(shootKey))
+        {
+            Shoot();
+        }
+
+        if (debug)
+        {
+            m_MousePos = Input.mousePosition;
+
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(m_MousePos);
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        m_MousePos = Input.mousePosition;
+
+        //m_MousePos.z = 12;
+
+        RaycastHit hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(m_MousePos);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
+            m_ShootDir = hit.point;
+
+            //m_ShootDir.z = m_Transform.position.z;
+
+            GameObject proj = Instantiate(projectile, m_Transform.position, Quaternion.identity);
+
+            proj.transform.LookAt(m_ShootDir);
+
+            proj.GetComponent<Rigidbody>().AddForce(proj.transform.forward * projSpeed, ForceMode.Impulse);
+        }
+    }
+}
