3047a1e15f41cfd0b2b4213bd57193a92be40dd0
cjabs <cjabs97@gmail.com>
2019-06-02 11:04:26 -0700

    Add damage/health for projectiles and players
    -Projectiles shoot and disappear as intended
    -Currently correctly lowers health across all clients
    -Bullets will collide with shooter...working on fix

---
+++ Assets/Scripts/Damager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool showDebug;
+
+    [SerializeField]
+    private int damage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        IDamageable damageableComponent = other.GetComponent<IDamageable>();
+        if(damageableComponent != null)
+        {
+            damageableComponent.LoseHealth(damage);
+
+            if(showDebug)
+            {
+                Debug.Log(other.name + " was hit and they have a damageableComponent.");
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/PhotonScripts/PlayerManager.cs
+++ Assets/Scripts/PhotonScripts/PlayerManager.cs
@@ -5,9 +5,15 @@ using Photon.Pun;
 using Photon.Realtime;
 
 
-public class PlayerManager : MonoBehaviourPunCallbacks
+public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable, IDamageable
 {
+    [Header("Settings")]
     public static GameObject LocalPlayerInstance;
+    public bool showDebug;
+
+    [Header("Debug")]
+    [SerializeField]
+    private int health = 1; // Maybe we can do this better if you only have one health? Not sure but this should work for now
 
 
     private void Awake()
@@ -30,5 +36,26 @@ public class PlayerManager : MonoBehaviourPunCallbacks
         if (!photonView.IsMine)
             return;
     }
-       
+     
+    public void LoseHealth(int deltaHealth)
+    {
+        if(showDebug)
+        {
+            Debug.Log(this.gameObject.name + " now has " + health);
+        }
+        health -= 1;
+    }
+
+    // Streams player health.
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(health);
+        }
+        else
+        {
+            this.health = (int)stream.ReceiveNext();
+        }
+    }
 }
